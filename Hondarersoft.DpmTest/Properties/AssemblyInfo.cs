71ea6ac5dd4abec55d5d20459eb95dd0b9c8d58b
Tetsuo Honda <t-honda@hondarer-soft.com>
2019-01-03 12:32:45 +0900

    テストプロジェクトの追加

---
+++ Hondarersoft.DpmTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Hondarersoft.DpmTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Hondarersoft.DpmTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2e467018-7aac-4a01-8dc7-94aadaaf1986")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Hondarersoft.DpmTest/ServiceProcess/Apis/ServiceProcessTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace Hondarersoft.Dpm.Apis.Tests
+{
+    [TestClass()]
+    public class ServiceProcessTests
+    {
+        [TestMethod()]
+        public void IsServiceSessionTest()
+        {
+            // Since we never run tests from the service session, we can not test all the paths.
+            if (Process.GetCurrentProcess().SessionId == 0)
+            {
+                Assert.AreEqual(true, ServiceProcess.IsServiceSession());
+            }
+            else
+            {
+                Assert.AreEqual(false, ServiceProcess.IsServiceSession());
+            }
+        }
+    }
+}
